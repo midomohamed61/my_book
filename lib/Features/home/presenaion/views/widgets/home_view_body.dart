678a774ec8761a96875b3dd34ce1ed6f86302eb5
your-githb-usernam <midomohamed61>
2024-02-11 18:59:54 +0200

    Best seller commit

--- lib/Features/home/presenaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presenaion/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_book/Features/home/presenaion/views/widgets/custom_app_bar.dart';
 import 'package:my_book/Features/home/presenaion/views/widgets/featured_books_list_view.dart';
+import 'package:my_book/core/utls/assets.dart';
 import 'package:my_book/core/utls/styles.dart';
 
 class HomeViewBody extends StatefulWidget {
@@ -26,6 +27,31 @@ class _HomeViewBodyState extends State<HomeViewBody> {
           Text('Best Seller',
             style: Styles.titleMedium,
           )
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 125,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          width: 100,
+          height: MediaQuery.of(context).size.height * .25,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(image: AssetImage(AssetsData.testImage))),
+        ),
+      );
         ],
       ),
     );
