8309ea8abd0e8638a9bd97a4d0b1f38ff29c1495
your-githb-usernam <midomohamed61>
2024-02-12 22:01:24 +0200

    Create custom book details appbar commit

--- lib/Features/home/presenaion/views/widgets/best_seller.dart
+++ lib/Features/home/presenaion/views/widgets/best_seller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_book/Features/home/presenaion/views/widgets/book_rating.dart';
+import 'package:my_book/core/utls/app_router.dart';
 import 'package:my_book/core/utls/assets.dart';
 import 'package:my_book/core/utls/styles.dart';
 class BestSellerListViewItem extends StatelessWidget {
@@ -8,50 +9,55 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.7 / 4,
-            child: Container(
-              width: 100,
-              height: MediaQuery.of(context).size.height * .25,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image:
-                  DecorationImage(image: AssetImage(AssetsData.testImage))),
+    return GestureDetector(
+      onTap: (){
+        GoRouter.of(context).push(AppRouter.KBookView);
+      },
+      child: Container(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                width: 100,
+                height: MediaQuery.of(context).size.height * .25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image:
+                    DecorationImage(image: AssetImage(AssetsData.testImage))),
+              ),
             ),
-          ),
-          const SizedBox(width: 30,),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width*.5,
-                child: Text("Harry Potter and the Goblet of fire",
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              const SizedBox(height: 3,),
-              Text('jk Rowling ',
-                style: Styles.textStyle14,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('19.99 \$',
-                    style: Styles.textStyle20.copyWith(
-                        fontWeight: FontWeight.bold
-                    ),
+            const SizedBox(width: 30,),
+            Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width*.5,
+                  child: Text("Harry Potter and the Goblet of fire",
+                    style: Styles.textStyle20,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  BookRating(),
-                ],
-              )
-            ],
-          )
-        ],
+                ),
+                const SizedBox(height: 3,),
+                Text('jk Rowling ',
+                  style: Styles.textStyle14,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('19.99 \$',
+                      style: Styles.textStyle20.copyWith(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    BookRating(),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/core/utls/app_router.dart
+++ lib/core/utls/app_router.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_book/Features/home/presenaion/views/home_view.dart';
+import 'package:my_book/Features/home/presenaion/views/widgets/book_details_view_body.dart';
 import 'package:my_book/Features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter{
+  static const KHomeView = '/homeView';
+  static const KBookView = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -11,8 +14,12 @@ abstract class AppRouter{
         builder: (context, state) =>const SplashView(),
       ),
       GoRoute(
-        path: '/homeView',
+        path: KHomeView,
         builder: (context, state) =>const HomeView(),
+      ),
+      GoRoute(
+        path: KBookView,
+        builder: (context, state) =>const BookDetailsViewBody(),
       ),
 
     ],
