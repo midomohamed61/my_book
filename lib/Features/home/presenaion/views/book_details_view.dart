34ef198593666ea7b7fdf100569a7e207a8ea0dd
your-githb-usernam <midomohamed61>
2024-02-12 20:23:16 +0200

    book details commit

---
+++ lib/Features/home/presenaion/views/book_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:my_book/Features/home/presenaion/views/widgets/book_detaol_view_body.dart';
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
