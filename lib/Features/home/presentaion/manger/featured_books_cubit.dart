fc9122efffb21e4220028e991d3de74b2ee024fe
your-githb-usernam <midomohamed61>
2024-03-06 18:14:57 +0200

    create fetchFeatureBooks commit

--- lib/Features/home/presentaion/manger/featured_books_cubit.dart
+++ lib/Features/home/presentaion/manger/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:my_book/Features/home/data/models/book_model.dart';
+import 'package:my_book/Features/home/data/repos/home_repo.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks()async{
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+
+      result.fold((failure) {
+        emit(FeaturedBooksFailure(failure.message));
+      }, (books) {
+        emit(FeaturedBooksSuccess(books));
+      }
+      );
+  }
 }
