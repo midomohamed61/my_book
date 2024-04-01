354f92adb2ee193913218b6585b28179e3783cff
your-githb-usernam <midomohamed61>
2024-04-01 23:27:55 +0200

    cached network image

--- lib/Features/home/presentaion/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentaion/views/widgets/custom_book_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:my_book/core/utls/assets.dart';
 
@@ -8,15 +9,13 @@ final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-            ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(imageUrl: imageUrl,
+        fit: BoxFit.fill,
+        errorWidget: (context, url, error) =>const  Icon(Icons.error),
         ),
       ),
     );
